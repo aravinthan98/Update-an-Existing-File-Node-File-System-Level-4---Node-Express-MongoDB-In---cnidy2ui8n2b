6c325e3d20c0fbf63e4fa853bfc27ba735de6127
aravinthan98 <117471432+aravinthan98@users.noreply.github.com>
2024-01-14 09:28:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  await fs.appendFile(fileName,fileContent);
+  const content=await fs.readFile(fileName,"utf-8");
+  return content;
   
 };
 
